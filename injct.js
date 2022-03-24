018fe417ec7a6c3d5c0955d71082d6352197ebd9
Touchlite <touch.lite.development@gmail.com>
2022-03-24 19:03:57 +0000

    Update injct.js

--- injct.js
+++ injct.js
@@ -1,13 +1,56 @@
-const argumentValue = process.argv[1];
+/* Variables */
+
+const args = process.argv
+
+const argumentValue = args[2];
 const colors = require('colors')
 
-function inject(array, strings)
+const defaultNoArgString = 
+[
+    'Sorry! But you need to setup an argument to do that!',
+]
+
+/* Not actually used in code, but for my memory :') */
+
+const commands =
+[
+    '-i',
+    '-st',
+    '-injct',
+    '-help',
+]
+
+/* Functions */
+
+function injectst(array, strings)
 {
-    for (x = 0; x <= strings.length; x++)
+    for (x = 0; x <= strings.length - 1; x++)
     {
         array[array.length] = strings[x];
         console.log('injct:'.green + ' injected ' + String(strings[x]).green + ' into selected array');
     }
 }
 
-inject(Array(), argumentValue)
+/* Main Code */
+
+if (argumentValue)
+{
+    if (argumentValue == '-i')
+    {
+        let path2 = String(args[3])
+        let path1 = String(args[4])
+
+        let split0 = path1.split("/")
+        let split1 = path2.split("/")
+
+        console.log('injct:'.green + ' injecting ' + String(split1[split1.length - 1]).green + ' to file: ' + String(split0[split0.length - 1]).green)
+    }
+    else if (argumentValue == '-injct')
+    {
+        console.log()
+    }
+}
+else
+{
+    console.log('injct:'.green + ' sorry! you need to specify an argument value for the function to work! Try again.'.red)
+}
